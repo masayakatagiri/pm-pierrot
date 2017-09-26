5beb383898e227a7c6ca91ba876e3c4f0145c616
masaya katagiri <masaya@idea-note.net>
2017-09-26 20:31:22 +0900

    hi

---
+++ db/migrate/20170926112629_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :rails
+      t.string :generate
+      t.string :scaffold
+      t.string :User
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
